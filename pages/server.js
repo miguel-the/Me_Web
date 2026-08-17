863e5091eba344df42bce518ba3578eeab316d7f
miguel-the <miguel.alonzo02@utrgv.edu>
2026-08-17 02:35:04 +0000

    Your custom commit message

--- pages/server.js
+++ pages/server.js
@@ -7,12 +7,22 @@ app.use(cors());
 
 let visitors = 0;
 
+app.get('/', (req, res) => {
+    res.send('Visitor counter is running!');
+});
+
 app.get('/visit', (req, res) => {
     visitors++;
+
+    console.log(`Visitor count: ${visitors}`);
 
     res.json({
         count: visitors
     });
 });
 
-app.listen(process.env.PORT || 3000);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
