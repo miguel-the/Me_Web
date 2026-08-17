0a15ff76011d3c43d7b39387cbe3f4ef4b937784
miguel-the <miguel.alonzo02@utrgv.edu>
2026-08-17 02:21:35 +0000

    Your custom commit message

--- pages/storage.js
+++ pages/storage.js
@@ -1,11 +1,8 @@
-    var n = localStorage.getItem('on_load_counter');
-
-    if (n === null) {
-        n = 0;
-    }
-
-    n++;
-
-    localStorage.setItem("on_load_counter", 0);
-
-    document.getElementById('CounterVisitor').innerHTML = n;
+fetch('https://me-web-tosx.onrender.com/visit')
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('CounterVisitor').innerHTML = data.count;
+    })
+    .catch(error => {
+        console.error('Error getting visitor count:', error);
+    });
